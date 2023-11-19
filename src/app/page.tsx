c61a6ddef802c5e9341f457d42dbf8e5753533cb
Marcos Willian <markusuuuu@gmail.com>
2023-11-19 19:24:23 -0300

    feat WIP: form structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { Mail } from 'lucide-react'
+
 import { SettingsTabs } from '@/components/SettingsTabs'
+import * as Input from '@/components/Input'
 
 export default function Home() {
   return (
@@ -6,6 +9,39 @@ export default function Home() {
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
 
       <SettingsTabs />
+
+      <div className="mt-6 flex flex-col">
+        <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo nad personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              form="settings"
+              className="rounded-lg bg-violet-600 px-4 py-2 font-semibold text-white shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form
+          id="settings"
+          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
+        >
+          form
+        </form>
+      </div>
     </>
   )
 }
